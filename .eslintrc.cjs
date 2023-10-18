9263c3637f783407ce16650292038c42d8bea4c6
Michael Carillon <Michael.S.Carillon@sherwin.com>
2023-10-18 15:16:51 -0400

    initial

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'vue/attribute-hyphenation': 'off',
+    'vue/v-on-event-hyphenation': 'off'
+  }
+};
